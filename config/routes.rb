d2e1b69cb04cf6d2ea0cd69700c7660e30c53fc2
Lucas Ferrari Soto <lucasmfsoto@gmail.com>
2021-07-19 13:24:15 -0300

    Change sign in and sign up routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'users#index'
   resources :users, only: [:index, :show]
+
+  devise_scope :user do
+    get 'log_in', to: 'devise/sessions#new'
+    get 'sign_up', to: 'devise/registrations#new'
+  end
 end
