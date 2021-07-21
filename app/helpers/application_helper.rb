b08a9130e38f1cae899716ecf846cbfadc68ef5a
Lucas Ferrari Soto <lucasmfsoto@gmail.com>
2021-07-21 16:08:13 -0300

    Add default user photo and user cover
    And helper methods to display them.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def user_pic(user, image_class: '')
+    image_tag(user.photo, class: image_class) if user.photo
+
+    image_tag(image_path('default_photo.jpg'), class: image_class)
+  end
+
+  def user_cover(user)
+    image_tag(user.cover_image, class: 'cover-image') if user.cover_image
+
+    image_tag(image_path('default_cover.jpg'), class: 'cover-image')
+  end
 end
