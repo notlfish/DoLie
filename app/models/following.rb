9de526c6bd4fa65f7c053968a15c777698402577
Lucas Ferrari Soto <lucasmfsoto@gmail.com>
2021-07-22 12:59:24 -0300

    Add Following validation
    Followings are validated for uniqueness on the combination of
    follower and followed.

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,4 +1,7 @@
 class Following < ApplicationRecord
   belongs_to :follower, class_name: 'User'
   belongs_to :followed, class_name: 'User'
+
+  validates :follower, uniqueness: { scope: :followed,
+                                     message: "You can't follow someone twice" }
 end
